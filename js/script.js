6c1c8879ed3010c381384797f49715afea58e2c1
markie_m <marzolfmarkie@gmail.com>
2021-12-03 11:38:44 -0600

    Renamed some variables for clarity

--- js/script.js
+++ js/script.js
@@ -1,27 +1,28 @@
 const overview = document.querySelector(".overview");
 const username = "markie-m";
 
-const ghInfo = async function () {
+const ghUserInfo = async function () {
     const response = await fetch(`https://api.github.com/users/${username}`);
 
-    const repos = await response.json();
-    console.log(repos);
-    displayUser(repos);
+    const data = await response.json();
+    console.log(data);
+    displayUserInfo(data);
 };
-ghInfo();
 
+ghUserInfo();
 
-const displayUser = function (repos) {
+const displayUserInfo = function (data) {
     const div = document.createElement("div");
     div.classList.add("user-info");
-    div.innerHTML = `<figure>
-    <img alt="user avatar" src=${repos.avatar_url} />
-  </figure>
-  <div>
-    <p><strong>Name:</strong> ${repos.name}</p>
-    <p><strong>Bio:</strong> ${repos.bio}</p>
-    <p><strong>Location:</strong> ${repos.location}</p>
-    <p><strong>Number of public repos:</strong> ${repos.public_repos}</p>
-  </div>`;
+    div.innerHTML = `
+    <figure>
+        <img alt="user avatar" src=${data.avatar_url} />
+    </figure>
+    <div>
+        <p><strong>Name:</strong> ${data.name}</p>
+        <p><strong>Bio:</strong> ${data.bio}</p>
+        <p><strong>Location:</strong> ${data.location}</p>
+        <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+    </div>`;
   overview.append(div);
 }
